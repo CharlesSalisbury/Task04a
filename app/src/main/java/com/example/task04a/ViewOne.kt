ebaf4d22a8f8f8b82cd53d4ebe5d37022a3b33ad
Charles <charlie_loves_skylines@hotmail.com>
2020-02-27 15:18:41 +0000

    Singletap added

--- app/src/main/java/com/example/task04a/ViewOne.kt
+++ app/src/main/java/com/example/task04a/ViewOne.kt
@@ -3,6 +3,9 @@ package com.example.task04a
 import android.content.Context
 import android.graphics.Color
 import android.util.AttributeSet
+import android.util.Log
+import android.view.GestureDetector
+import android.view.MotionEvent
 import android.view.View
 
 class ViewOne: View {
@@ -13,10 +16,36 @@ class ViewOne: View {
         attrs,
         defStyleAttr
     )
+    // Instantiate the GestureDetector with the application context
+    // and an implementation of the GestureDetector.OnGestureListener interface
+    private val myGestureDetector = GestureDetector(context, myGestureListener())
+
+
     init {
 // 'this' is a reference to the current class.
 // Views have many properties - one is backgroundColor.
         this.setBackgroundColor(Color.argb(128,32,64,255))
     }
+
+    override fun onTouchEvent(ev: MotionEvent): Boolean {
+        return myGestureDetector.onTouchEvent(ev) || super.onTouchEvent(ev)
+    }
+
+    inner class myGestureListener: GestureDetector.SimpleOnGestureListener() {
+        // You should always include onDown() and it should always return true.
+        // Otherwise the GestureListener may ignore other events.
+        override fun onDown(ev: MotionEvent): Boolean {
+            return true
+        }
+        override fun onSingleTapUp(ev: MotionEvent): Boolean {
+            Log.d(LOGTAG, "SingleTapUp")
+            return true
+        }
+    } // End of myGestureListener class
+
+    companion object { // declare a constant (must be in the companion)
+        const val LOGTAG = "MyTask"
+    }
+
 
 }
